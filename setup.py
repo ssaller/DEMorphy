7ae6e86cbd1cf0092a038d83dd4fdb55ddce26ec
ssaller <smf.saller@gmail.com>
2023-07-10 14:07:11 +0200

    add pull first

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
+import os
+import subprocess
+import sys
+
+try:
+    import git
+except ModuleNotFoundError:
+    subprocess.call([sys.executable, '-m', 'pip', 'install', 'gitpython'])
+    import git
 
 
 def get_version():
@@ -15,6 +24,19 @@ install_requires = [
     "dawg2 (>= 0.7.8)"
 ]
 
+def pull_first():
+    """This script is in a git directory that can be pulled."""
+    cwd = os.getcwd()
+    gitdir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(gitdir)
+    g = git.cmd.Git(gitdir)
+    try:
+        g.execute(['git', 'lfs', 'pull'])
+    except git.exc.GitCommandError:
+        raise RuntimeError("Make sure git-lfs is installed!")
+    os.chdir(cwd)
+
+pull_first()
 
 setup(
     name="demorphy",
